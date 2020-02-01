b630eee5706c5c0798fb4dece118270238ffc8c2
José Márquez Doblas <jose.marquez@atlantyasoftware.com>
2020-02-01 19:04:53 +0100

    Bad line fixed

--- static/js/main.js
+++ static/js/main.js
@@ -2,12 +2,10 @@
 
 function showReplyForm(commentid) {
     var formid = "form-" + commentid
-
-    console.log("HOLAAAAA")
+    
     if (document.getElementById(formid).style.display === "none") {
       document.getElementById(formid).style.display = "block";
     } else {
       document.getElementById(formid).style.display = "none";
-    }
-      
+    } 
 }
